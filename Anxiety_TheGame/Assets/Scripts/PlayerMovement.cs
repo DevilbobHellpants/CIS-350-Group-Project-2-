7c6e79012b8b532561c873942e73fa5ea342e29d
AbstractThink81 <89855045+AbstractThink81@users.noreply.github.com>
2022-04-05 17:09:46 -0500

    Added player animations

--- Anxiety_TheGame/Assets/Scripts/PlayerMovement.cs
+++ Anxiety_TheGame/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Jacob Zydorowicz
+ * Jacob Zydorowicz, Ian Connors
  * Project 5
  * Controls player movement and actions in overworld
  */
@@ -12,6 +12,7 @@ public class PlayerMovement : MonoBehaviour
     public float xSpeed;
     public float ySpeed;
     public float walkSpeed;
+    public Animator Anim;
   
 
     // Update is called once per frame
@@ -26,6 +27,33 @@ public class PlayerMovement : MonoBehaviour
         transform.Translate(Vector2.up * Time.deltaTime * walkSpeed * ySpeed);
 
 
+        /*WalkDir:
+         * 0: None
+         * 1: Up
+         * 2: Down
+         * 3: Right
+         * 4: Left
+         */
+        if (xSpeed == 0 && ySpeed == 0)
+		{
+            Anim.SetInteger("WalkDir", 0);
+		}
+        else if (xSpeed > 0.5f)
+		{
+            Anim.SetInteger("WalkDir", 3);
+        }
+        else if (xSpeed < -0.5f)
+        {
+            Anim.SetInteger("WalkDir", 4);
+        }
+        else if (ySpeed > 0)
+        {
+            Anim.SetInteger("WalkDir", 1);
+        }
+        else if (ySpeed < 0)
+        {
+            Anim.SetInteger("WalkDir", 2);
+        }
     }
 }
 
