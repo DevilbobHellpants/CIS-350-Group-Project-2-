ed3bae12144538015143d3acecc8b29deaf1cda2
Pyroghost0 <89740009+Pyroghost0@users.noreply.github.com>
2022-04-05 10:33:59 -0500

    Reset The Game With Gitignore

---
+++ Anxiety_TheGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Jacob Zydorowicz
+ * Project 5
+ * Controls player movement and actions in overworld
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float xSpeed;
+    public float ySpeed;
+    public float walkSpeed;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        xSpeed = Input.GetAxis("Horizontal");
+        ySpeed = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector2.right * Time.deltaTime * walkSpeed * xSpeed);
+
+
+        transform.Translate(Vector2.up * Time.deltaTime * walkSpeed * ySpeed);
+
+
+    }
+}
+
