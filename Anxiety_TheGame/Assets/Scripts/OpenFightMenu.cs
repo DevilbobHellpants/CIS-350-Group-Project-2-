7a529991788cf5860f25a974ae6248a6ecd520f9
abreuker <abreuker@mail.bradley.edu>
2022-04-05 22:42:38 -0500

    ui fight menu and start of OpenFightMenu script

---
+++ Anxiety_TheGame/Assets/Scripts/OpenFightMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/*
+ * Anna Breuker
+ * Project 5 
+ * Opens the fight menu when player touches a cloud.
+ */
+
+//this is supposed to be attached to the cloud prefab- might recode it to be attached to the player because this "FindGameObjectWithTag" isn't finding the game object with tag.
+public class OpenFightMenu : MonoBehaviour
+{
+    public GameObject fightMenu;
+
+    void Start()
+    { 
+        fightMenu = GameObject.FindGameObjectWithTag("FightMenu");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            fightMenu.SetActive(true);
+            Debug.Log("cloud hit");
+            Time.timeScale = 0f;
+        }
+        Destroy(gameObject);
+    }
+}
