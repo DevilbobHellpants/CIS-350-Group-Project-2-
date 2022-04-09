43bc613ad7506fae5f1a5f242dac903341a00bb6
Pyroghost0 <89740009+Pyroghost0@users.noreply.github.com>
2022-04-09 12:01:47 -0500

    Added Overworld Anxiety Effects

---
+++ Anxiety_TheGame/Assets/Scripts/OverworldAnxietyEffect.cs
@@ -0,0 +1,115 @@
+﻿/* Caleb Kahn
+ * Project 5
+ * While the player is in the overworld, this script increases the effects of anxiety over time
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
+
+public class OverworldAnxietyEffect : MonoBehaviour
+{
+    public GameObject[] imagePrefabs;
+    public bool inBattle = false;//I can take this value from somewhere else
+    public Image darknessEffect;
+    public CinemachineVirtualCamera cinemachine;
+    public GameObject player;
+    private float timer = 0;
+    private float randomTimer = 0;
+    private float cooldownTimer = 0;
+    private float minCooldown = 3f;
+    private float pov = 3.5f;
+    private int randomChance = 120;
+    private float effectTimer = 3f;
+    private float alpha = 0f;
+    private float minAlpha = 0f;
+    private float maxAlpha = .1f;
+    private float alphaChangeTime = 2f;
+    private bool alphaUp = true;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cinemachine.m_Lens.OrthographicSize = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        randomTimer += Time.deltaTime;
+        cooldownTimer += Time.deltaTime;
+        if (!inBattle)
+        {
+            if (timer <= 30)
+            {
+                minCooldown = 3f - (1f * (timer / 30f));
+                pov = 3.5f - (.5f * (timer / 30f));
+                cinemachine.m_Lens.OrthographicSize = pov;
+                randomChance = (int)(120f - (65f * (timer / 30f)));
+                effectTimer = 3f + (1.5f * (timer / 30f));
+                minAlpha = .3f * (timer / 30f);
+                maxAlpha = .1f + (.4f * (timer / 30f));
+            }
+            else if (timer <= 60)
+            {
+                minCooldown = 2f - (1.5f * ((timer - 30f) / 30f));
+                pov = 3f - (.5f * ((timer - 30f) / 30f));
+                cinemachine.m_Lens.OrthographicSize = pov;
+                randomChance = (int)(65f - (40f * ((timer - 30f) / 30f)));
+                effectTimer = 4.5f + (1.5f * ((timer - 30f) / 30f));
+                minAlpha = .2f + (.3f * ((timer - 30f) / 30f));
+                maxAlpha = .5f + (.5f * ((timer - 30f) / 30f));
+            }
+            else
+            {
+                effectTimer = 6f + (1.5f * ((timer - 60f) / 30f));
+            }
+            if (randomTimer >= .1f && cooldownTimer >= minCooldown) {
+                randomTimer = 0f;
+                if (0 == Random.Range(0, randomChance)) {
+                    cooldownTimer = 0f;
+                    int num = Random.Range(0, imagePrefabs.Length);
+                    GameObject effect = Instantiate(imagePrefabs[num], new Vector2(Random.Range(-8f, 6.0f) + player.transform.position.x, Random.Range(-5f, 5.0f) + player.transform.position.y), imagePrefabs[num].transform.rotation);
+                    effect.GetComponent<OverworldEffectMovement>().effectTimer = effectTimer;
+                }
+            }
+            if (alphaUp)
+            {
+                alpha += (maxAlpha - minAlpha) / alphaChangeTime * Time.deltaTime;
+                if (alpha > maxAlpha)
+                {
+                    alpha = maxAlpha;
+                    alphaUp = false;
+                }
+            }
+            else
+            {
+                alpha -= (maxAlpha - minAlpha) / alphaChangeTime * Time.deltaTime;
+                if (alpha < minAlpha)
+                {
+                    alpha = minAlpha;
+                    alphaUp = true;
+                }
+            }
+            darknessEffect.color = new Color(0f, 0f, 0f, alpha);
+        }
+    }
+
+    public void resetVariables()
+    {
+        timer = 0;
+        randomTimer = 0;
+        cooldownTimer = 0;
+        minCooldown = 3f;
+        pov = 3.5f;
+        randomChance = 100;
+        effectTimer = 3f;
+        alpha = 0f;
+        minAlpha = 0f;
+        maxAlpha = .1f;
+        alphaUp = true;
+    }
+}
---
+++ Anxiety_TheGame/Assets/Scripts/OverworldEffectMovement.cs
@@ -0,0 +1,75 @@
+﻿/* Caleb Kahn
+ * Project 5
+ * Allows effects to move
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OverworldEffectMovement : MonoBehaviour
+{
+    public float speed = 3f;
+    public float effectTimer = 5f;
+    public Image image;
+    public Text text;
+
+    private float timer = 0f;
+    private Vector3 direction;
+
+    private void Start()
+    {
+        direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+        speed = Random.Range(1f, 4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= effectTimer)
+        {
+            Destroy(this.gameObject);
+        }
+        transform.position += direction * speed * Time.deltaTime;
+        if (image == null)
+        {
+            if (effectTimer < 5f)
+            {
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f - (Mathf.Abs(effectTimer / 2 - timer) / (effectTimer / 2)));
+            }
+            else
+            {
+                if (timer < 2.5f)
+                {
+                    gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f - (2.5f - timer) / (2.5f));
+                }
+                else if (effectTimer - 2.5f < timer)
+                {
+                    gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f - (2.5f - (effectTimer - timer)) / (2.5f));
+                }
+            }
+        }
+        else
+        {
+            if (effectTimer < 5f)
+            {
+                image.color = new Color(1f, 1f, 1f, 1f - (Mathf.Abs(effectTimer / 2 - timer) / (effectTimer / 2)));
+                text.color = new Color(0f, 0f, 0f, 1f - (Mathf.Abs(effectTimer / 2 - timer) / (effectTimer / 2)));
+            }
+            else
+            {
+                if (timer < 2.5f)
+                {
+                    image.color = new Color(1f, 1f, 1f, 1f - (2.5f - timer) / (2.5f));
+                    text.color = new Color(0f, 0f, 0f, 1f - (2.5f - timer) / (2.5f));
+                }
+                else if (effectTimer - 2.5f < timer)
+                {
+                    image.color = new Color(1f, 1f, 1f, 1f - (2.5f - (effectTimer - timer)) / (2.5f));
+                    text.color = new Color(0f, 0f, 0f, 1f - (2.5f - (effectTimer - timer)) / (2.5f));
+                }
+            }
+        }
+    }
+}
