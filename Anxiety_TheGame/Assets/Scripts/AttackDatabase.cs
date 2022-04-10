eae4d84749ebcb6821b361085e5bbb64b6be34ec
DevilbobHellpants <trillizio3@hotmail.com>
2022-04-10 15:18:39 -0500

    Attack Database

---
+++ Anxiety_TheGame/Assets/Scripts/AttackDatabase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Attack Database", menuName = "Combat System/Database")]
+public class AttackDatabase : MonoBehaviour
+{
+    public AttackObjects[] AttackObjects;
+
+    [ContextMenu("Update ID's")]
+    public void UpdateID()
+    {
+        for (int i = 0; i < AttackObjects.Length; i++)
+        {
+            if (AttackObjects[i].data.Id != i)
+            {
+                AttackObjects[i].data.Id = i;
+            }
+        }
+    }
+    public void OnAfterDeserialize()
+    {
+        UpdateID();
+    }
+
+    public void OnBeforeSerialize()
+    {
+    }
+}
