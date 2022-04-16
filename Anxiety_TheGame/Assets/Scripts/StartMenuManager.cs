e8b90b2c99c12c3cc12c9a024a39241b6569ec2f
Pyroghost0 <89740009+Pyroghost0@users.noreply.github.com>
2022-04-16 15:51:11 -0500

    Start Menu Added

---
+++ Anxiety_TheGame/Assets/Scripts/StartMenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartMenuManager : MonoBehaviour
+{
+    public TextMeshProUGUI titleText;
+    public TextMeshProUGUI storyText;
+    public TextMeshProUGUI continueText;
+    private int phase = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && phase == 0)
+        {
+            titleText.gameObject.SetActive(false);
+            storyText.gameObject.SetActive(true);
+            continueText.text = "<Space To Start Game>";
+            phase++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && phase == 1)
+        {
+            SceneManager.LoadScene("TilemapTest");
+        }
+    }
+}
