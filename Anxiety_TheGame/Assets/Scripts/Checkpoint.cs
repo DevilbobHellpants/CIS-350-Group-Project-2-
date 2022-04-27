fd37badf2d962fe1016de9ab48b1fb27382247b0
AbstractThink81 <89855045+AbstractThink81@users.noreply.github.com>
2022-04-27 16:27:36 -0500

    Added animated checkpoint

---
+++ Anxiety_TheGame/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Ian Connors
+ * Project 5 
+ * Animates the checkpoint
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint: MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.SetBool("Opened", false);
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        if (collision.gameObject.CompareTag("Player"))
+            anim.SetBool("Opened", true);
+    }
+}
