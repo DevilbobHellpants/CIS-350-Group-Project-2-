b88a18e9b0a0d7ca5cd2fe2700b439655e1088a5
abreuker <abreuker@mail.bradley.edu>
2022-04-29 16:56:17 -0500

    new description box when hovering over attack buttons
    will implement the changing text later tonight or tomorrow.

---
+++ Anxiety_TheGame/Assets/Scripts/HoverDescriptionVisable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/*
+ * Name: Anna Breuker
+ * Project: Project 7
+ * Description: Makes an UI appear when mouse hovers over attack buttons. Code adapted from https://www.youtube.com/watch?v=uUXmbYVFWME&t=47s
+ */
+
+public class HoverDescriptionVisable : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public GameObject descriptionCanvas;
+    private bool mouseOver = false;
+
+    //TODO
+    //---going to put code and variables here that reads what
+    //   enemy they're fighting/the attack on this button, and
+    //   change the description text based on that. 
+
+    void Start()
+    {
+        descriptionCanvas.SetActive(false);
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouseOver = true;
+        descriptionCanvas.SetActive(true);
+        //Debug.Log("Mouse enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouseOver = false;
+        descriptionCanvas.SetActive(false);
+        //Debug.Log("Mouse exit");
+    }
+}
+
---
+++ Anxiety_TheGame/Assets/Scripts/UIFollowMouse.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * Name: Anna Breuker
+ * Project: Project 7
+ * Description: Makes an UI object follow the mouse. Code adapted from https://www.youtube.com/watch?v=JBn9cJvTJnA
+ */
+
+public class UIFollowMouse : MonoBehaviour
+{
+
+    public RectTransform MovingObject;
+    public Vector3 offset;
+    public RectTransform BasisObject;
+    public Camera cam;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveObject();
+    }
+
+    public void MoveObject()
+    {
+        Vector3 pos = Input.mousePosition + offset;
+        pos.z = BasisObject.position.z;
+        MovingObject.position = cam.ScreenToWorldPoint(pos);
+    }
+}
+
