a08190233afef43d43e4a387e37397a2cfe84cd8
abreuker <abreuker@mail.bradley.edu>
2022-05-01 15:10:15 -0500

    Hovering attack description box fully implemented

--- Anxiety_TheGame/Assets/Scripts/HoverDescriptionVisable.cs
+++ Anxiety_TheGame/Assets/Scripts/HoverDescriptionVisable.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 /*
@@ -13,6 +14,8 @@ public class HoverDescriptionVisable : MonoBehaviour, IPointerEnterHandler, IPoi
 {
 
     public GameObject descriptionCanvas;
+    public Text displayText;
+    private OpenFightMenu enemy;
     private bool mouseOver = false;
 
     //TODO
@@ -23,6 +26,7 @@ public class HoverDescriptionVisable : MonoBehaviour, IPointerEnterHandler, IPoi
     void Start()
     {
         descriptionCanvas.SetActive(false);
+        enemy = GameObject.FindGameObjectWithTag("Player").GetComponent<OpenFightMenu>();
     }
 
 
@@ -30,6 +34,88 @@ public class HoverDescriptionVisable : MonoBehaviour, IPointerEnterHandler, IPoi
     {
         mouseOver = true;
         descriptionCanvas.SetActive(true);
+        displayText.text = "This is a test!";
+        //Debug.Log(attackButtons[i].GetComponentInChildren<Text>().text);
+
+        if (this.tag == "Attack 1")
+        {
+            if (enemy.enemyNameDisplayed.text == "Glass Eye")
+            {
+                displayText.text = "\"Ground\" yourself in the present moment, decreasing your anxiety and chipping away at the problem at hand.";
+            }
+            else if (enemy.enemyNameDisplayed.text == "Liar Smiler")
+            {
+                displayText.text = "\"Ground\" yourself in the present moment, decreasing your anxiety and chipping away at the problem at hand.";
+            }
+            else if (enemy.enemyNameDisplayed.text == "Scramble Sound")
+            {
+                displayText.text = "Sometimes, loud music can help drown out your thoughts, decreasing your anxiety and chipping away at the problem at hand.";
+            }
+            else if (enemy.enemyNameDisplayed.text == "Question Air")
+            {
+                displayText.text = "Hide from your problems, anxiety staying the same but ending the encounter early.";
+            }
+
+        }
+        else if (this.tag == "Attack 2")
+        {
+            if (enemy.enemyNameDisplayed.text == "Glass Eye")
+            {
+                displayText.text = "Gather support from friends, decreasing your anxiety and chipping away at the problem at hand.";
+            }
+            else if (enemy.enemyNameDisplayed.text == "Liar Smiler")
+            {
+                displayText.text = "Decreasing your anxiety, but at the cost of increasing your problems. And some other... unintended side effects.";
+            }
+            else if (enemy.enemyNameDisplayed.text == "Scramble Sound")
+            {
+                displayText.text = "Take a few deep breaths, decreasing your anxiety and chipping away at the problem at hand.";
+            }
+            else if (enemy.enemyNameDisplayed.text == "Question Air")
+            {
+                displayText.text = "Shift your focus elsewhere, decreasing your anxiety and chipping away at the problem at hand.";
+            }
+        }
+        else if (this.tag == "Attack 3")
+        {
+            if (enemy.enemyNameDisplayed.text == "Glass Eye")
+            {
+                displayText.text = "Give in, increase your anxiety, but escape from the encounter early.";
+            }
+            else if (enemy.enemyNameDisplayed.text == "Liar Smiler")
+            {
+                displayText.text = "Sometimes you just need to sleep it off. Decrease anxiety, but at the cost of a few turns.";
+            }
+            else if (enemy.enemyNameDisplayed.text == "Scramble Sound")
+            {
+                displayText.text = "Sometimes you just need to leave the room. End encounter early.";
+            }
+            else if (enemy.enemyNameDisplayed.text == "Question Air")
+            {
+                displayText.text = "This is too much- shut down, increase anxiety, end encounter early.";
+            }
+        }
+        else if (this.tag == "Attack 4")
+        {
+            if (enemy.enemyNameDisplayed.text == "Glass Eye")
+            {
+                displayText.text = "Glasses are for nerds anyways. Decrease anxiety, but at the expense of your sight.";
+            }
+            else if (enemy.enemyNameDisplayed.text == "Liar Smiler")
+            {
+                displayText.text = "Who needs friends anyways? Increase anxiety, but end encounter early.";
+            }
+            else if (enemy.enemyNameDisplayed.text == "Scramble Sound")
+            {
+                displayText.text = "Get out some pent up rage! Decrease anxiety, but increase problem health.";
+            }
+            else if (enemy.enemyNameDisplayed.text == "Question Air")
+            {
+                displayText.text = "Visualize yourself somewhere else. Decrease problem health.";
+            }
+        }
+
+        //displayText = 
         //Debug.Log("Mouse enter");
     }
 
